d1b22ef2172f84ec413149a1bc5572cc10e9c2df
Riodhani23 <144421088+Riodhani23@users.noreply.github.com>
2023-10-12 13:20:03 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// variabel teks uppercase
+let teks = 'Merah Putih';
+let uppercase = teks.toUpperCase();
+alert(uppercase);
+// variabel teks lowercase
+let bendera = 'Merah Putih';
+let lowercase = bendera.toLowerCase();
+alert(lowercase);
+// mengubah variabel dalam bentuk teks
+let Teks = 'Merah Putih';
+let ubahTeks = Teks.replace('Putih', 'Muda');
+alert(ubahTeks);
+// ukuran panjang variabel
+let Mood = 'Merah Putih';
+let lenghtpanjang = Mood.length;
+alert(lenghtpanjang);
